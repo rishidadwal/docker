b5964d9eb99adff268cccb05b36d33d5a33dee8f
Rishi <1990rishi@gmail.com>
2019-10-12 23:49:32 +0530

    --- Auto-Git Commit ---

--- stack/db-setup/run.sh
+++ stack/db-setup/run.sh
@@ -3,5 +3,6 @@ if [ -z "MYSQL_ROOT_PASSWORD"]; then
 exit 1
 fi
 
+elsse 30
 curl -s  https://s3-us-west-2.amazonaws.com/studentapi-cit/studentapp-ui-proj1.sql > /tmp/studentapp.sql
 mysql -h mariadb -uroot -p root < /tmp/studentapp.sql
