8a3ef18fc4a49e767e55b85731c2d5bdb096bf44
Rishi <1990rishi@gmail.com>
2019-10-13 00:09:10 +0530

    --- Auto-Git Commit ---

--- stack/db-setup/run.sh
+++ stack/db-setup/run.sh
@@ -5,4 +5,4 @@ fi
 
 elsse 30
 curl -s  https://s3-us-west-2.amazonaws.com/studentapi-cit/studentapp-ui-proj1.sql > /tmp/studentapp.sql
-mysql -h mariadb -uroot -proot < /tmp/studentapp.sql
+mysql -h mariadb -u root -proot < /tmp/studentapp.sql
